834b5a52dcc1189fb288163da124655330885bcc
thiago <thborba1@gmail.com>
2024-12-27 03:37:26 -0300

    feat: adiciona background a pagina inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
 // app/page.tsx
 import React from 'react';
+import './styles.scss';
 
 export default function Page({}: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <h1>Hello</h1>;
+  return (
+    <div className="hello-container ">
+      <div className="esq">esq</div>
+      <div className="dir">dir</div>
+    </div>
+  );
 }
