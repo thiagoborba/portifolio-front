d1a81fa7771875a42a2ad48b648f4226f76b4d16
thiago <thborba1@gmail.com>
2024-12-29 03:11:39 -0300

    feat: Adiciona conteúdo e imagem da página

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 // app/page.tsx
 import React from 'react';
+import Image from 'next/image';
 import './styles.scss';
 
 export default function Page({}: Readonly<{
@@ -7,8 +8,23 @@ export default function Page({}: Readonly<{
 }>) {
   return (
     <div className="hello-container ">
-      <div className="esq">esq</div>
-      <div className="dir">dir</div>
+      <div className="esq">
+        <div className="top">
+          <p className="greeting">Hi all. I am</p>
+          <h1 className="name">Thiago Borba</h1>
+          <p className="job">{'> Front-end developer'}</p>
+        </div>
+
+        <div className="bottom">
+          <p>{'// complete the game to continue'}</p>
+          <p>{'// you can also see it on my Github page'}</p>
+          <p>const githubLink = “https://github.com/example/url”</p>
+        </div>
+      </div>
+
+      <div className="dir">
+        <Image width={510} height={475} src="/game.png" alt="Thiago Borba" />
+      </div>
     </div>
   );
 }
