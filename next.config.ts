2e11d17587856c3b68f9990d9e3345768b81524a
thiago borba <thiago.borba@dbccompany.com.br>
2025-04-22 22:22:59 -0300

    feat: Adiciona config para lidar com alias no buid

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,19 @@
 import type { NextConfig } from 'next';
+import path from 'path';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  webpack: (config: WebpackConfig) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
+};
+
+interface WebpackConfig {
+  resolve: {
+    alias: { [key: string]: string };
+  };
+}
+
+module.exports = {};
 
 export default nextConfig;
