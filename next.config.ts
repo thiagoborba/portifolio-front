94ee012cdc22209fbc091361a1ce37491dd82c1c
Thiago Borba <thiagoborbadev@gmail.com>
2026-02-11 17:28:16 -0300

    refactor: remove configuração de alias do webpack

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,5 @@
 import type { NextConfig } from 'next';
-import path from 'path';
 
-const nextConfig: NextConfig = {
-  webpack: (config: WebpackConfig) => {
-    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
-    return config;
-  },
-};
-
-interface WebpackConfig {
-  resolve: {
-    alias: { [key: string]: string };
-  };
-}
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
