1630cf22ce59a0c4a93abc18e2b0eaa0c22c19a8
thiagoborba <thiagoborbadev@gmail.com>
2026-03-13 11:54:52 -0300

    feat: Integra com backend o envio do formulario de contato

--- src/app/contact-me/components/Form/index.tsx
+++ src/app/contact-me/components/Form/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { Input, Textarea, Button } from '@/Components';
 import { useFormContext } from '../../context/FormContext';
@@ -12,15 +12,45 @@ export interface Inputs {
 }
 
 export const ContactForm = () => {
-  const { values, setValues, setValue } = useFormContext();
+  const [loading, setLoading] = useState(false);
+
+  const { values, setValue } = useFormContext();
   const { register, handleSubmit } = useForm<Inputs>({
     defaultValues: values,
   });
 
   const onSubmit = (data: Inputs) => {
-    setValues(data);
-    console.log(data, 'dados enviados');
+    submitForm(data);
   };
+
+  async function submitForm(data: Inputs) {
+    setLoading(true);
+    try {
+      const { email, message, name } = data;
+      const payload = {
+        name,
+        email,
+        message,
+      };
+
+      const response = await fetch('http://localhost:8000/contact-form', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(payload),
+      });
+
+      const result = await response.json();
+      if (response.ok) {
+        alert('Mensagem enviada com sucesso!');
+      } else {
+        alert('Erro: ' + result.error);
+      }
+    } catch (error: unknown) {
+      console.error('Error submitting form:', error);
+    } finally {
+      setLoading(false);
+    }
+  }
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
@@ -46,7 +76,9 @@ export const ContactForm = () => {
         placeholder="your message here..."
         label="_message:"
       />
-      <Button type="submit">submit-message</Button>
+      <Button type="submit" disabled={loading}>
+        {loading ? '...loading' : 'submit-message'}
+      </Button>
     </form>
   );
 };
